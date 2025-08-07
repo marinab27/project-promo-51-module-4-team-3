5b6d90ee66cfbd43a16df6ef4889bf10813e391c
Marina Boyano <marina.boyano.gonzalez@gmail.com>
2025-08-07 21:41:45 +0200

    cambios

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,62 +1,89 @@
-import projectModel from '../models/projectModel.js'
+import projectModel from "../models/projectModel.js";
 
 //funcion para listar todos los proyectos
 const listProjects = async (req, res) => {
-    
-    //pedir datos: lo hace el modelo
-    const projectsList = await projectModel.getAll(); 
+  //pedir datos: lo hace el modelo
+  const projectsList = await projectModel.getAll();
 
-    //dar respuesta
-    res.status(200).json({success: true, results: projectsList})
-}
+  //dar respuesta
+  res.status(200).json({ success: true, results: projectsList });
+};
 
 //funcion para listar un proyecto específico
 const listOneProject = async (req, res) => {
+  const id = req.params.id; //el id se busca aquí
 
-    const id = req.params.id;//el id se busca aquí
-    
-    const oneProject = await projectModel.getOne(id);//y se le pasa a la función getOne del modelo
+  const oneProject = await projectModel.getOne(id); //y se le pasa a la función getOne del modelo
 
-    if(oneProject.length===0) {
-        res.status(404).json({
-            success: false, 
-            mensaje: "⚠️El ID no coincide con ninguna frase"})
-    }
-    else { 
-        res.status(200).json({success: true, results: oneProject})
-    }
-}
+  if (oneProject.length === 0) {
+    res.status(404).json({
+      success: false,
+      mensaje: "⚠️El ID no coincide con ninguna frase",
+    });
+  } else {
+    res.status(200).json({ success: true, results: oneProject });
+  }
+};
 
 //funcion para añadir autor y proyecto AL MISMO TIEMPO
 //crear autor --> obtener id autor --> crear proyecto vinculado al id del autor --> devolver datos
 const addAuthorAndProject = async (req, res) => {
+  const {
+    author,
+    job,
+    photo,
+    name,
+    slogan,
+    repo,
+    demo,
+    technologies,
+    desc,
+    image,
+  } = req.body; //viajan por el body //OJO: no puedo enviar el id por el body ya que no existe aun y lo voy a crear a la vez que creo el autor
 
-    const {author, job, photo, name, slogan, repo, demo, technologies, description, image} = req.body;//viajan por el body //OJO: no puedo enviar el id por el body ya que no existe aun y lo voy a crear a la vez que creo el autor
+  if (
+    !author ||
+    !job ||
+    !photo ||
+    !name ||
+    !slogan ||
+    !repo ||
+    !demo ||
+    !technologies ||
+    !desc ||
+    !image /* || !author_id */
+  ) {
+    res.status(400).json({
+      success: false,
+      mensaje: "⚠️Faltan campos obligatorios",
+    });
+  }
 
-    if(!author || !job || !photo || !name || !slogan || !repo || !demo || !technologies || !description || !image /* || !author_id */){
-            res.status(400).json({
-            success: false, mensaje: "⚠️Faltan campos obligatorios",
-            })
-    }
+  //1. añadir un autor
 
-    //1. añadir un autor
-  
-    const newAuthor = await projectModel.addAuthor(author, job, photo);//y se le pasa a la función addAuthor del modelo
-    
-    const author_id = newAuthor.insertId;//me quedo con el id y es lo que tengo que unir con el proyecto
+  const newAuthor = await projectModel.addAuthor(author, job, photo); //y se le pasa a la función addAuthor del modelo
 
-    //2. añadir proyecto
-    
-    const newProject = await projectModel.addProject(name, slogan, repo, demo, technologies, description, image, author_id);//y se le pasa a la función addProject del modelo
+  const author_id = newAuthor.insertId; //me quedo con el id y es lo que tengo que unir con el proyecto
 
-    res.status(200).json({
-            success: true,
-            mensaje: "✅Autor y proyecto añadidos correctamente",
-            author_id,
-            project_id: newProject.insertId
-            })
-  
-}
+  //2. añadir proyecto
 
-export default {listProjects, listOneProject, addAuthorAndProject};
+  const newProject = await projectModel.addProject(
+    name,
+    slogan,
+    repo,
+    demo,
+    technologies,
+    desc,
+    image,
+    author_id
+  ); //y se le pasa a la función addProject del modelo
 
+  res.status(200).json({
+    success: true,
+    mensaje: "✅Autor y proyecto añadidos correctamente",
+    author_id,
+    project_id: newProject.insertId,
+  });
+};
+
+export default { listProjects, listOneProject, addAuthorAndProject };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
-import express from 'express'; 
-import cors from 'cors'; 
-import projectRoute from './routes/projectRoute.js'; //importar las rutas
-import dotenv from 'dotenv'; 
+import express from "express";
+import cors from "cors";
+import projectRoute from "./routes/projectRoute.js"; //importar las rutas
+import dotenv from "dotenv";
 dotenv.config();
 
-const server = express(); 
+const server = express();
 const port = process.env.PORT || 4000;
+server.use(
+  express.json({
+    limit: "50mb",
+  })
+);
 
-server.use(express.json()); 
-server.use(cors()); 
-server.use('/project', projectRoute);//le digo al servidor que use las rutas
-server.set('view engine', 'ejs');//EJS
+server.use(express.json());
+server.use(cors());
+server.use("/project", projectRoute); //le digo al servidor que use las rutas
+//server.set('view engine', 'ejs');//EJS
 
 //escuchar el puerto
 server.listen(port, () => {
-    console.log(`Servidor escuchando por http://localhost:${port}`);
-})
+  console.log(`Servidor escuchando por http://localhost:${port}`);
+});
 
+/* server.get("/detail/:id", async (req, res) => {
+    const conn = await getConnection();
+    let sql = "SELECT authors.*, form.* FROM form INNER JOIN authors ON form.author_id = authors.id WHERE form.id = ?";
 
+    const [result] = await conn.query(sql); 
+    await conn.end();
+
+    res.render("showProjects", { data: result }); 
+}); */
